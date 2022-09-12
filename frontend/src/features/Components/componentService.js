d88b45e9364a459446bd4acf9427a1dffba7ba6e
anudeep652 <76202842+anudeep652@users.noreply.github.com>
2022-09-12 10:39:45 +0530

    update api url

--- frontend/src/features/Components/componentService.js
+++ frontend/src/features/Components/componentService.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const URL = "https://components100.herokuapp.com";
+const URL = "https://components-164g.onrender.com";
 //get all components
 export const getComponents = async () => {
   try {
